e401410f7cae5bc65667bccc4e438541f73ef4cb
André Lubawski <andrelubawski97@outlook.com>
2022-02-10 00:12:22 -0300

    day 1-100 days of code

---
+++ 100-days-of-code/day-1/01.py
@@ -0,0 +1,28 @@
+"""print("Day 1 - Python Print Function is Working")"""
+
+'''print("Hello\nWorld")
+print("\tHello" + "\n\tWorld")'''
+
+'''###############input function#####################'''
+
+"""print('Hello ' + input('your name is? '))"""
+
+"""
+name = input("What is your name? ")
+
+print(f'My name is {name}')
+"""
+
+"""name = input('Your name is? ')
+print(len(name))
+"""
+'''print(len(input("Your name is: ")))'''
+
+a = input('a: ')
+b = input('b: ')
+c=a
+a=b
+b=c
+
+print('b: ',b)
+print('a: ',a)
